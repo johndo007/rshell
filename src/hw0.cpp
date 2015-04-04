6fa1824705fa9fdd2868214b98808e2911336188
Andrew So <aso001@ucr.edu>
2015-04-03 18:38:53 -0700

    1.02 Determining user cmd

--- src/hw0.cpp
+++ src/hw0.cpp
@@ -1,9 +1,20 @@
 #include<iostream>
 #include<unistd.h>
-#include<sys/type.h>
-#include<sys/wait.h>
-
+//#include<sys/type.h>
+//#include<sys/wait.h>
+#include<cstdlib>
+#include<cstdio>
 using namespace std;
+
+void list_cmd(string& cmds)	//converts string into individal commands
+{
+	
+	///check for connectors {|| or && or ;}
+	//convert to string to char
+	char buff[1028];
+	
+	
+}
 
 int main(int argc, char *argv[])
 {
@@ -27,12 +38,16 @@ int main(int argc, char *argv[])
 		getline(cin, commands);					//user input commands
 		if (commands.find("exit") != std::string::npos) 
 		{
-			exit(0);							//exit if prompted
+			exit(0);							//exit if prompted by user
+			//return 0;
 		}
 		
 		
 		///testing user input
-		cout<<cmd<<endl;
+		cout<<commands<<endl;
+		
+		/// break up commands
+		list_cmd(commands);
 	}
 
   return 0;
