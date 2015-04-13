c32288b7321db3e3beb49cf3f16364eb7279a70f
MisterSoandSo <aso001@ucr.edu>
2015-04-13 15:34:11 -0700

    1.09 - Improved cmd_list() and changed from queue to vector

--- src/hw0.cpp
+++ src/hw0.cpp
@@ -1,100 +1,124 @@
-#include<iostream>
-#include<unistd.h>
-#include<sys/types.h>
-#include<sys/wait.h>
-#include<cstdlib>
-#include<cstdio>
-#include<queue>
+#include <iostream>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <vector>
+//#include <queue>
+#include <string>
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+
 using namespace std;
+
+bool printErrors = false;
+
+int totalWordCount = 0;
 
 void prompt()
 {
-	if (getlogin() != NULL) 				//only runs if there is a login 
-	{	
-		cout << getlogin() << "@";			//display login and @
-	}
-	char hostname[127] = {0};	        	//max bytes is 128 in hostname
-	if (gethostname(hostname, 255) != -1)   //if there is a hostname
-	{
-		gethostname(hostname, 255);			//put hostname -> hostname
-		cout << hostname << " ";			//cout host name
-	}
-	cout << "$" << " ";						//prompt $
+    if (getlogin() != NULL) //only runs if there is a login
+    {
+    cout << getlogin() << "@"; //display login and @
+    }
+    char hostname[127] = {0}; //max bytes is 128 in hostname
+    if (gethostname(hostname, 255) != -1) //if there is a hostname
+    {
+    gethostname(hostname, 255); //put hostname -> hostname
+    cout << hostname << " "; //cout host name
+    }
+    cout << "$" << " "; //prompt $
 }
 
-void list_cmd(string& cmds, queue<string>&str_cmd)	//converts string into individal commands
+void list_cmd(string& input, vector<string>&words) //converts string into individal commands
 {
-	///convert to string to cmds
-	
-	string temp_cmd = "";	//temporary command
-	unsigned int str_len = cmds.size();	//string size	
-	
-	for(int j=0;j<str_len;j++)
+    vector<string> v; 
+	int cnt = 0, wordCnt = 0; 
+	string word; 
+	for(string::iterator it=input.begin(); it != input.end(); it++)
 	{
-		temp_cmd += cmds[j];
-		///check for connectors {|| or && or ;}	
-		if(cmds[j]==';')
+		if(input.at(cnt) == '#')
 		{
-			str_cmd.push(temp_cmd);
-			temp_cmd="";
-			if(cmds[j++]==' ') j++;
+			if(word != " " && !word.empty())
+			{
+				v.push_back(word);
+			}
+			v.push_back(";");	
+			word = "";
+			break;
 		}
-		if(j==(str_len-1)) str_cmd.push(temp_cmd);	
+		else if(input.at(cnt) == ' ') //case1 - space
+		{
+		    if(!word.empty() && word != "" && word != " ") 
+		    {
+			    v.push_back(word);
+			    word = "";
+			    totalWordCount++;
+		    }
+		    else 
+            { 
+                cout<<"Hello World"<<endl; 
+            } 
+	    }
+	    else if(input.at(cnt) == ';') //case2 - semicolon
+        {
+            if(word != "" && word != " ") 
+            {
+                v.push_back(word); 
+                totalWordCount++;
+            }
+            v.push_back(";"); 
+            word = ""; // reset word
+        }
+        else
+		{
+            word += input.at(wordCnt);
+    	}
+		wordCnt++;
+		cnt++;
 	}
+	
+	if(word != "" && word != " ")
+	{
+		v.push_back(word);
+		if(word != ";")
+		{
+			v.push_back(";");
+		}
+		totalWordCount++;
+	}
+	totalWordCount++;
+	if(v.at(v.size()-1) != ";") 
+    { 
+        v.push_back(";"); 
+    }
+	totalWordCount = v.size();
+	words = v;
+    
 }
 
-void exec_cmd(queue<string> &str_cmd)
+void exec_cmd(vector<string>&words)
 {
-	//begin executing commands
-	int cmdnum =0;
-	while(!str_cmd.empty())
-	{
-		cout<<cmdnum<<endl;
-		string curr_cmd = str_cmd.front();
-
-		int pid = fork();
-		if(pid == -1)	//error value is -1
-		{
-			perror("fork ");
-			exit(1);
-		}
-		else if(pid == 0)
-		{
-			cout<<"This is the child process ";
-			//if(execvp("ls",argv) == -1)
-			//{
-			//	perror("Command failed to run ");
-			//	exit(1);
-			//}
-			
-			exit(0);
-		}
-
-		str_cmd.pop();					//remove command after execution
-		cmdnum++;
-	}
-	
+        //Being reworked as of 1.09 update
+        cout<<"Sorry execvp is unavailable for user usage"<<endl;
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char** argv)
 {
+	string input;
+    vector<string>words;
 	
-	queue<string>cmd;
-	string commands;
-
-	while (true)								//keep asking user for commands
+	while(true)
 	{
-		prompt();								//Setup user prompt
-				
-		getline(cin, commands);					//user input commands
+        prompt(); //Setup user prompt
+      	
+		getline (cin, input);		
+		       
+        ///break up commands
+        list_cmd(input, words);
 
-		/// break up commands
-		list_cmd(commands,cmd);					//function handles user input into cmds
-		
-		/// execute commands
-		exec_cmd(cmd);
+        ///execute commands        
+        exec_cmd(words);
 	}
-
-  return 0;
-  
+	return 0;
 }
