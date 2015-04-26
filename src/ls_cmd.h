0c43e1e823b7add751a039e66a8251e364c2a9dd
Andrew So <aso001@hammer.cs.ucr.edu>
2015-04-26 00:05:35 -0700

    1.25 - Added color text and finished up -l

--- src/ls_cmd.h
+++ src/ls_cmd.h
@@ -10,6 +10,9 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <grp.h>
+#include <pwd.h>
+#include <cstdint>
 
 using namespace std;
 
@@ -44,41 +47,97 @@ vector<string> read_filenames(string &path)
 	return filenames;
 
 }
-void make_string(string &path, string &fname, string &output)
+void get_perms(mode_t mode, char buf[])
 {
-    string fullpath;
+	char ftype = '?';
+	if(S_ISREG(mode)) ftype = '-';
+	if(S_ISDIR(mode)) ftype = 'd';
+	sprintf(buf, "%c%c%c%c%c%c%c%c%c%c",ftype,
+			mode & S_IRUSR ? 'r' : '-',
+			mode & S_IWUSR ? 'r' : '-',
+			mode & S_IXUSR ? 'r' : '-',
+			mode & S_IRGRP ? 'r' : '-',
+			mode & S_IWGRP ? 'r' : '-',
+			mode & S_IXGRP ? 'r' : '-',
+			mode & S_IROTH ? 'r' : '-',
+			mode & S_IWOTH ? 'r' : '-',
+			mode & S_IXOTH ? 'r' : '-');
+	
+}
+
+void get_color_code (mode_t st_mode, char *color_fmt)
+{
+	if(S_ISDIR(st_mode))
+	{
+		sprintf(color_fmt, "\x1b[34m");
+	}
+	else if (st_mode & S_IXUSR)
+	{
+		sprintf(color_fmt, "\x1b[32m");
+	}
+}
+
+string make_string(string &path, string &fname, string &output, int &mode)
+{
+    struct stat statBuf;
+	char fstatus[16];
+	char tmp[32];
+	struct passwd *pwd;
+	struct group *grp;
+	struct tm *tm;
+
+	string fullpath;
     fullpath = path;
     fullpath += '/';
     fullpath += fname;
     output = ""; //clear
 
-    if(stat(full.c_str(), &statBuf)==-1)    return output;
+    if(stat(fullpath.c_str(), &statBuf)==-1)    return output;
 
     get_perms(statBuf.st_mode, fstatus);
     output = fstatus;
-    sprintf(tmp,"%d", statBuf.st_link);
+	output +=' ';
+    sprintf(tmp,"%d", statBuf.st_nlink);
     output += tmp;
-
-    if((pwd=getwuid(statBuf.st_uid))!=NULL)
+	output += ' ';
+	//get owner's name
+    if((pwd=getpwuid(statBuf.st_uid) )!=NULL)
         sprintf(tmp,"%-s", pwd->pw_name);
     else
         sprintf(tmp,"%-d", statBuf.st_uid);
     output += tmp;  //-perm-#-user-user
-
-    if((grp=getgrid(statBuf.st_gid))!=NULL)
+	output += ' ';
+	//get groups's name
+    if((grp=getgrgid(statBuf.st_gid))!=NULL)
         sprintf(tmp,"%-s", grp->gr_name);
     else
         sprintf(tmp,"%-d", grp->gr_name);
     output += tmp;
-
-    sprintf(tmp, "%7jd", (int max_t)statBuf.st_size);
+	output += ' ';
+	//get file size
+    sprintf(tmp, "%8jd", (intmax_t)statBuf.st_size);
     output += tmp;
-
+	output += ' ';
+	//get Month:Day:Hour::Minute
     char datestring[256];
-    char fmt = "%b %d %H: %M";
-    tm = localtime(&statBuf, st_mtime);
+    char fmt[] = "%b %d %H: %M";
+    tm = localtime(&statBuf.st_mtime);
     strftime(datestring, sizeof(datestring),fmt,tm);
     output += datestring;
+	output += ' ';
+	char color_fmt[32];
+	color_fmt[0] = 0;
+	char default_fmt[]= "\x1b[39;49m";
+	if(fname[0] == '.')
+	{
+		sprintf(color_fmt,"%s", "\x1b[34;47m");
+	}
+	else
+	{
+		get_color_code(statBuf.st_mode, color_fmt);
+	}
+	sprintf(tmp, "%s%s%s", color_fmt, fname.c_str(), default_fmt);
+	output += tmp;
 }
 
 void print_ALL(string &path, vector<string> &v, int &mode)
@@ -93,9 +152,9 @@ void print_ALL(string &path, vector<string> &v, int &mode)
 		//print  -al
 		for(int x = 0; x <v.size();x++)
         {
-            string ouput;
+            string output;
             make_string(path, v[x], output, mode);
-            cout<<output;
+            cout<<output<<endl;
         }
 
 	}
@@ -124,7 +183,7 @@ void print_LONG(string &path, vector<string> &v, int &mode)
 	{
         if(v[x][0] !='.')   //no hidden files
         {
-            string ouput;
+            string output;
             make_string(path, v[x], output, mode);
             cout<<output;
         }
@@ -144,7 +203,8 @@ void ls_cmd(char** argv)
 	int x = 1;
 	int mode = 0;
 	string path = ".";
-	vector<string> v;
+	vector<string> v; 
+	bool flag = true;
 	//vector<bool> flag;
 	//for(int i = 0; i < 3;i++)  flag.push_back(false);
 	/*for(int i = 0 ;argv[i]!=NULL ;i++)
@@ -159,7 +219,7 @@ void ls_cmd(char** argv)
 		//cout<<"ls_cmd1  was called"<<endl;
 		if(argv[x][0]!='-' && x!=0)
 		{
-			cout<<"argv[x]="<<argv[x]<<endl;
+			//cout<<"argv[x]="<<argv[x]<<endl;
 			path = argv[x];
 		}
 		//first part of parameter uses '-'
@@ -196,22 +256,36 @@ void ls_cmd(char** argv)
 				cout<<v[i]<<endl;
 			}*/
 
-			if(mode & DIR_ALL) print_ALL(path,v,mode);	//if -a is true
+			if((mode & DIR_ALL)&&(flag==true))
+			{ 
+				print_ALL(path,v,mode);	//if -a is true
+				flag = false;
+			}
 
-			if(mode & DIR_LONG) print_LONG(path,v,mode);	//if -l is true
+			if((mode & DIR_LONG)&&(flag==true)) 
+			{ 
+				print_LONG(path,v,mode);	//if -l is true
+				flag = false;
+			}
 
-			if(mode & DIR_R) print_R(path,v,mode);	//if -R is true
+			if((mode & DIR_R)&&(flag==true))
+			{
+				print_R(path,v,mode);	//if -R is true
+				flag = false;
+			}
 
 		}
 		else
 		{
 			//assume path name
-			path = argv[0];
+		    //path = argv[0];
 			v = read_filenames(path);
+			
 			for(int i = 0; i<v.size();i++)
 			{
-				cout<<v[i]<<endl;
+				if(v[i][0]!= '.') cout<<v[i]<<' ';
 			}
+			cout<<endl;
 		}
 
 	}
