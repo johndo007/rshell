396a983192682076b496e7651f4f1f3c36bc3739
Andrew So <aso001@ucr.edu>
2015-04-30 10:08:50 -0700

    1.23

--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,15 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-	string input;	//get user cmd in one line
-    vector<string>words;	//seperate the user input into vector of strings
+	string input;		//get user cmd in one line
+    	vector<string>words;	//seperate the user input into vector of strings
 	int wordcnt = 0;	//word counter to keep track of postion
 
 	while(true)
 	{
         prompt(); //Setup user prompt
 
-		getline (cin, input);
+	getline (cin, input);
 
         ///break up commands into vector<string>
         list_cmd(input, words,wordcnt);
