e478beaaad8426f44cb4a4dc0ef10db90ec2984c
Andrew So <aso001@hammer.cs.ucr.edu>
2015-05-11 14:14:39 -0700

    1.42 - Updated

--- src/redirect.h
+++ src/redirect.h
@@ -1,29 +1,16 @@
-<<<<<<< HEAD
 #ifndef _REDIRECT_H__
 #define _REDIRECT_H__
 
 #include <iostream>
-#include <stat.h>
 #include <sys/unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <fcntl.h>
 #include <errno.h>
 #include <algorithm>
-#include <>
-
 
 using namespace std;
 
-void redirection()
-{
-
-}
-
-
 // <,>,>>,|
-
 #endif // _REDIRECT_H__
-=======
-
 
