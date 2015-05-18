983fbad1e4195745d12783d602633abdec77eb0c
Andrew <aso001@ucr.edu>
2015-05-17 22:38:15 -0700

    1.50b - update .h to .cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <unistd.h>
 #include <vector>
 #include <string>
-#include "shell.h"
-
+#include <cstring>
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <algorithm>
+#include <pwd.h>
+#include <grp.h>
+#include <dirent.h>
+#include <sys/ioctl.h>	//Terminal width info
 using namespace std;
+
+void prompt(string &input);   //get user info ... DONE
+vector<string> extract_ls_cmd (string& input) ;
+void exec_cmd(vector<string>&xwords);
+
+
 
 int main(int argc, char** argv)
 {
@@ -13,7 +33,9 @@ int main(int argc, char** argv)
 	while(true)
 	{
         prompt(input);				//Setup user prompt and input
-	    words = list_cmd(input);	//return vector<string>
+       
+	    words = extract_ls_cmd(input);	//return vector<string>
+	   
         exec_cmd(words);			//execute command
 	}
 	return 0;
